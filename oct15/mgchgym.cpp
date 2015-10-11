ea54d854c66ba1810b8db12db342f15313e41f6c
Ashutosh Nath Agarwal <ash7594@gmail.com>
2015-10-11 16:52:57 +0530

    oct 10 partial

--- oct15/mgchgym.cpp
+++ oct15/mgchgym.cpp
@@ -5,6 +5,8 @@
 #define gc getchar_unlocked
 using namespace std;
 
+//bool comp(const pair<int,int> &a,const pair<int,int> &b) { return a.first < b.first; }
+
 int inp() {
       char c = gc();
         while(c<'0' || c>'9') c = gc();
@@ -16,7 +18,72 @@ int inp() {
             return ret;
 }
 
-string check(iter l, iter r, int W) {
+bool check(iter l, iter r, int W) {
+	vector<bool> v1(W+1);
+	vector<int> w1(1,0);
+	w1.insert(w1.end(),l,r);
+
+//	map<int,int> hash;
+//	map<int,int>::iterator it1;
+//	REP(i,0,w1.size()) {
+//		it1 = hash.find(w1[i]);
+//		if (it1 == hash.end())
+//			hash.insert(it1,make_pair(w1[i],1));
+//		else
+//			(it1->second)++;
+//	}
+//	vector<pair<int,int> > w;
+//	REP(i,0,hash.size()) {
+//		if (hash[i] != 0)
+//			w.push_back(i);
+//	}
+	
+//	for (iter it=w1.begin(); it!=w1.end(); it++) {
+//		cout<<*it<<" ";
+//	}
+//	nl;
+
+	vector<pair<int,int> > w(1,make_pair(0,1));
+	vector<pair<int,int> >::iterator it;
+	REP(i,1,w1.size()) {
+		it = lower_bound(w.begin(),w.end(),make_pair(w1[i],0));
+		if (it!=w.end() && it->first == w1[i])
+			it->second++;
+		else
+			w.insert(it,make_pair(w1[i],1));
+	}
+	//cout<<w.size()<<endl;
+	vector<vector<bool> > v(w.size(),v1);
+
+	REP(i,0,w.size()) {
+		v[i][0] = true;
+	}
+	REP(i,1,W+1) {
+		v[0][i] = false;
+	}
+
+	//map<int,int>::iterator it = hash.begin();
+
+//	for (it=w.begin(); it!=w.end(); it++) {
+//		cout<<it->first<<" "<<it->second;
+//		nl;
+//	}
+
+	REP(i,1,w.size()) {
+		REP(j,0,W+1) {
+			v[i][j] = v[i-1][j];
+			if (j>=w[i].first) {
+				for (int k=1;k<=w[i].second && j>=k*w[i].first;k++) {
+					v[i][j] = v[i][j] || v[i-1][j-k*w[i].first];
+				}
+			}
+		}
+		if (v[i][W]) return true;
+	}
+	return false;
+}
+
+string check1(iter l, iter r, int W) {
 	vector<bool> v1(W+1);
 	vector<int> w(1,0);
 	w.insert(w.end(),l,r);
@@ -64,8 +131,11 @@ int main () {
 
 		if (a == 3) {
 			W = inp();
-			printf("%s",check(w.begin()+l,w.begin()+r+1,W).c_str());
-			nl;
+			if(check(w.begin()+l,w.begin()+r+1,W)) {
+				printf("Yes\n");
+			} else {
+				printf("No\n");
+			}
 		}
 	}
 	return 0;
