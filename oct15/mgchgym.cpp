bffa33f8329dd70f7d7e62487d199c758f0f5b2f
Ashutosh Nath Agarwal <ash7594@gmail.com>
2015-10-25 05:18:16 +0530

    rope try, slow on reverse

--- oct15/mgchgym.cpp
+++ oct15/mgchgym.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
+#include<ext/rope>
 #define nl printf("\n")
 #define REP(i,b,c) for(int i=b;i<c;i++)
 #define iter vector<int>::iterator
+#define iter2 rope<int>::iterator
 #define gc getchar_unlocked
 using namespace std;
-
-//bool comp(const pair<int,int> &a,const pair<int,int> &b) { return a.first < b.first; }
+using namespace __gnu_cxx;
 
 int inp() {
       char c = gc();
@@ -18,42 +19,22 @@ int inp() {
             return ret;
 }
 
-bool check(iter l, iter r, int W) {
+string check(iter2 l, iter2 r, int W) {
 	vector<bool> v1(W+1);
 	vector<int> w1(1,0);
 	w1.insert(w1.end(),l,r);
 
-//	map<int,int> hash;
-//	map<int,int>::iterator it1;
-//	REP(i,0,w1.size()) {
-//		it1 = hash.find(w1[i]);
-//		if (it1 == hash.end())
-//			hash.insert(it1,make_pair(w1[i],1));
-//		else
-//			(it1->second)++;
-//	}
-//	vector<pair<int,int> > w;
-//	REP(i,0,hash.size()) {
-//		if (hash[i] != 0)
-//			w.push_back(i);
-//	}
-	
-//	for (iter it=w1.begin(); it!=w1.end(); it++) {
-//		cout<<*it<<" ";
-//	}
-//	nl;
-
-	vector<pair<int,int> > w(1,make_pair(0,1));
-	vector<pair<int,int> >::iterator it;
-	REP(i,1,w1.size()) {
-		it = lower_bound(w.begin(),w.end(),make_pair(w1[i],0));
-		if (it!=w.end() && it->first == w1[i])
-			it->second++;
-		else
-			w.insert(it,make_pair(w1[i],1));
+	vector<int> hash(10001,0);
+	REP(i,0,w1.size()) {
+		hash[w1[i]]++;
 	}
-	//cout<<w.size()<<endl;
-	vector<bitset<10001> > v(w.size());
+	vector<int> w;
+	REP(i,0,10001) {
+		if (hash[i] != 0)
+			w.push_back(i);
+	}
+
+	vector<vector<bool> > v(w.size(),v1);
 
 	REP(i,0,w.size()) {
 		v[i][0] = true;
@@ -62,25 +43,18 @@ bool check(iter l, iter r, int W) {
 		v[0][i] = false;
 	}
 
-	//map<int,int>::iterator it = hash.begin();
-
-//	for (it=w.begin(); it!=w.end(); it++) {
-//		cout<<it->first<<" "<<it->second;
-//		nl;
-//	}
-
 	REP(i,1,w.size()) {
 		REP(j,0,W+1) {
 			v[i][j] = v[i-1][j];
-			if (j>=w[i].first) {
-				for (int k=1;k<=w[i].second && j>=k*w[i].first;k*=2) {
-					v[i][j] = v[i][j] || v[i-1][j-k*w[i].first];
+			if (j>=w[i]) {
+				for (int k=1;k<=hash[w[i]] && j>=k*w[i];k++) {
+					v[i][j] = v[i][j] || v[i-1][j-k*w[i]];
 				}
 			}
 		}
-		if (v[i][W]) return true;
+		if (v[i][W]) return "Yes";
 	}
-	return false;
+	return "No";
 }
 
 string check1(iter l, iter r, int W) {
@@ -111,31 +85,28 @@ int main () {
 	int n,q,a,l,r,W;
 	n = inp();
 	q = inp();
-	vector<int> w(n+1);
+	rope<int> w(n+1);
 	REP(i,1,n+1) {
-		w[i] = inp();
+		w.replace(i,inp());
 	}
 	while (q--) {
 		a = inp();
 		l = inp();
 		r = inp();
 		if (a == 1) {
-			w[l] = r;
+			w.replace(l,r);
 			continue;
 		}
 
 		if (a == 2) {
-			reverse(w.begin()+l,w.begin()+r+1);
+			reverse(w.mutable_begin()+l,w.mutable_begin()+r+1);
 			continue;
 		}
 
 		if (a == 3) {
 			W = inp();
-			if(check(w.begin()+l,w.begin()+r+1,W)) {
-				printf("Yes\n");
-			} else {
-				printf("No\n");
-			}
+			printf("%s",check(w.mutable_begin()+l,w.mutable_begin()+r+1,W).c_str());
+			nl;
 		}
 	}
 	return 0;
