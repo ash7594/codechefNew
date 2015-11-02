7b8fb8321644de08b3e9466172b67836513b63a7
Ashutosh Nath Agarwal <ash7594@gmail.com>
2015-11-02 19:01:02 +0530

    strsub partial

--- practice/strsub.cpp
+++ practice/strsub.cpp
@@ -92,21 +92,22 @@ int main() {
 			} else cause = -1;
 
 			if (cause == -1) {
-				if (a.back().second != n-1)
-					a.push_back(make_pair(i,n-1));
+				//if (a.back().second != n-1)
+				while(i<n)
+					a.push_back(make_pair(i++,n-1));
 				break;
 			} else {
 				j-=2;
-				if (a.back().second != j)
+				//if (a.back().second != j)
 					a.push_back(make_pair(i,j));
 			}
 		}
 
-		vector<long long> subseq(a.size()),ss(a.size());
-		REP(i,0,a.size()) {
-			cout<<"("<<a[i].first<<","<<a[i].second<<")";
-			nl;
-		}
+		vector<long long> subseq(a.size()),ss(a.size()),common(a.size()-1),scommon(a.size()-1);
+//		REP(i,0,a.size()) {
+//			cout<<"("<<a[i].first<<","<<a[i].second<<")";
+//			nl;
+//		}
 
 		long long count;
 		REP(i,0,subseq.size()) {
@@ -116,11 +117,30 @@ int main() {
 
 		ss[0] = subseq[0];
 		REP(i,1,ss.size()) {
-			count = 0;
-			if (a[i].first <= a[i-1].second)
-				count = (a[i-1].second-a[i].first+1);
-			ss[i] = ss[i-1] + subseq[i] - count*(count+1)/2;
+//			count = 0;
+//			if (a[i].first <= a[i-1].second)
+//				count = (a[i-1].second-a[i].first+1);
+			ss[i] = ss[i-1] + subseq[i];// - count*(count+1)/2;
 		}
+
+		REP(i,0,common.size()) {
+			if (a[i+1].first <= a[i].second) {
+				count = a[i].second - a[i+1].first + 1;
+				common[i] = count*(count+1)/2;
+			}
+		}
+
+		scommon[0] = common[0];
+		REP(i,1,scommon.size()) {
+			scommon[i] = scommon[i-1] + common[i];
+		}
+
+//		copy(ss.begin(),ss.end(),ostream_iterator<long long>(cout," "));
+//		nl;
+//		copy(common.begin(),common.end(),ostream_iterator<long long>(cout," "));
+//		nl;
+//		copy(scommon.begin(),scommon.end(),ostream_iterator<long long>(cout," "));
+//		nl;
 
 		long long ans;
 		while(q--) {
@@ -132,21 +152,27 @@ int main() {
 			iti iter1 = lower_bound(a.begin(),a.end(),make_pair(l,0),[](const pair<int,int> &a, const pair<int,int> &b)->bool{return a.first<b.first;});
 			iti iter2 = upper_bound(a.begin(),a.end(),make_pair(0,r),[](const pair<int,int> &a, const pair<int,int> &b)->bool{return a.second<b.second;});
 
-			if (iter1 == a.end() || iter2 == a.begin()) {
+			if (iter2 == a.begin()) {
 				ans = (r-l+1)*(r-l+2)/2;
 				write(ans);
 				continue;
 			}
 			iter2--;
 
-			if ((iter1-a.begin()-1) < 0) ans = ss[iter2-a.begin()];
-			else ans = ss[iter2-a.begin()] - ss[iter1-a.begin()-1];
-
-			if (iter1->first != l) {
-				iter1--;
-				count = iter1->second - l + 1;
-				ans += count*(count+1)/2;
+			if (iter1->second >= r) {
+				ans = (r-l+1)*(r-l+2)/2;
+				write(ans);
+				continue;
 			}
+
+			if ((iter1-a.begin()-1) < 0) ans = ss[iter2-a.begin()] - scommon[iter2-a.begin()-1];
+			else ans = ss[iter2-a.begin()] - ss[iter1-a.begin()-1] - (scommon[iter2-a.begin()-1] - scommon[iter1-a.begin()-1]);
+
+//			if (iter1->first != l) {
+//				iter1--;
+//				count = iter1->second - l + 1;
+//				ans += count*(count+1)/2;
+//			}
 
 			if (iter2->second != r) {
 				iter2++;
